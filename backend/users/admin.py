a649590571c6a0fc90776d725c45ab7ab5355961
Valeriy Uvarov <v.uvarov@tcsbank.ru>
2024-07-04 10:13:44 +0300

    add authtoken, admin panel and app api

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,3 +6,8 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'first_name', 'last_name', 'email', 'role']
     list_filter = ['email', 'first_name']
+
+
+admin.site.site_header = 'Administration'
+admin.site.site_title = 'Administration'
+admin.site.index_title = 'Administration'
